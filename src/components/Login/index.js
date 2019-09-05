97274f40bf464a91ce1ec9ffec727cfee8cecc1b
gccunanan <gccunanan@gmail.com>
2019-09-04 19:18:15 -0700

    Added components folder with Nav, Login, & Home compnents/pages to start off with

---
+++ src/components/Login/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../App.css';
+
+function Login() {
+  return (
+    <div>
+      <h1>This is the Login Page!</h1>
+    </div>
+  );
+}
+
+export default Login;
---
+++ src/components/Nav/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../../App.css';
+
+function Nav() {
+  return (
+    <nav>
+      <h3>Logo</h3>
+      <ul>
+        <li>Home</li>
+        <li>Login</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
