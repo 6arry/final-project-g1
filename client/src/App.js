e46e80db90274ec09c930f5ed43f2a8c7d160230
gccunanan <gccunanan@gmail.com>
2019-09-07 00:41:14 -0700

    react and server running at the same time; added bootstrap reactstrap for ui

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Djnavbar from './components/Djnavbar';
+
+function App() {
+  return (
+    <div className='App'>
+      <Djnavbar />
+    </div>
+  );
+}
+export default App;
