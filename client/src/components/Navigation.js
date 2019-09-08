a447da5faa3c46009a6b881b1665a1081182d23e
gccunanan <gccunanan@gmail.com>
2019-09-07 17:10:18 -0700

    fixed import "../App.css"

---
+++ client/src/components/Navigation.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../App.css';
+
+function Navigation() {
+  return (
+    <nav>
+      <h3>Logo</h3>
+      <ul>
+        <li>Home</li>
+        <li>Host</li>
+        <li>Join</li>
+        <li>Logout</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navigation;
