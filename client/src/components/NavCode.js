f1b1aed062c92e58076444bb823b8809e59a0305
gccunanan <gccunanan@gmail.com>
2019-09-09 15:40:23 -0700

    navigation ui up with router working

---
+++ client/src/components/NavCode.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Menu, Segment } from 'semantic-ui-react';
+
+export default class Navigation extends React.Component {
+  state = { activeItem: 'home' };
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+
+  render() {
+    const { activeItem } = this.state;
+
+    return (
+      <Segment inverted>
+        <Menu inverted pointing secondary>
+          <Menu.Item
+            name='home'
+            active={activeItem === 'home'}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name='messages'
+            active={activeItem === 'messages'}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name='friends'
+            active={activeItem === 'friends'}
+            onClick={this.handleItemClick}
+          />
+        </Menu>
+      </Segment>
+    );
+  }
+}
