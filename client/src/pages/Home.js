15dccec3d6eb16aa23f415ee86d3731115b67951
gccunanan <gccunanan@gmail.com>
2019-09-09 22:06:39 -0700

    added react-youtube dependency; hardcoded a video into the player with the video id; updated the favicon; added youtube player to home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import '../App.css';
 import SearchBar from '../components/SearchBar';
+import Ytplayer from '../components/Ytplayer';
 
 function Home() {
   return (
     <div>
       <h1>This is the Home page!</h1>
       <SearchBar />
+      <Ytplayer />
     </div>
   );
 }
