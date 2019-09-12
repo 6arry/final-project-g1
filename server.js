9584514ab561c5813bfb48dc7606593771532ec0
John Robinson <john.robinson.0710@gmail.com>
2019-09-11 17:30:45 -0700

    new commit

--- server.js
+++ server.js
@@ -13,12 +13,13 @@ const db = require('./config/keys').mongoURI;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
 mongoose
     .connect(db)
     .then(() => console.log('Mongo DB Connected...'))
     .catch(err =>console.log(err));
 
-app.use('./routes/api/items', items);
+//app.use('./routes/api/items', items);
 
     
     app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
