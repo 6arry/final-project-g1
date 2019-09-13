a508d0b1de12c72f4fc3105cebed250a643a3600
John Robinson <john.robinson.0710@gmail.com>
2019-09-12 19:27:52 -0700

    nochange

--- server.js
+++ server.js
@@ -1,30 +1,23 @@
-// eslint-disable-next-line no-unused-vars
+//eslint-disable-next-line no-unused-vars
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 
 const PORT = process.env.PORT || 3000;
 
 const routes = require('./routes/api/items');
+
 app.use(routes);
 
-
-
 const db = require('./config/keys').mongoURI;
-
-
-
 
 mongoose
     .connect(db)
     .then(() => console.log('Mongo DB Connected...'))
     .catch(err =>console.log(err));
 
-//app.use('./routes/api/items', items);
-
-    
     app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
