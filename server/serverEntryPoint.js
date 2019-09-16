791df40d3d118e958829cece066f112f5c21920d
Adam Grodahl <adamgrodahl@Adams-MBP.hsd1.or.comcast.net>
2019-09-15 22:06:12 -0700

    serverEntryPoint.js cleanup

--- server/serverEntryPoint.js
+++ server/serverEntryPoint.js
@@ -1,9 +1,8 @@
-// require("dotenv").config();
+//Server entry point 
+
 var express = require("express");
-// var exphbs = require("express-handlebars");
 var db = require("./models");
 var app = express();
-var bodyParser = require("body-parser");
 var http = require("http").createServer(app);
 var PORT = process.env.PORT || 3000;
 
@@ -28,16 +27,5 @@ if (process.env.NODE_ENV === "test") {
 http.listen(PORT, function() {
   console.log("Server started: http://localhost:%s/", PORT);
 });
-
-// Starting the server, syncing our models ------------------------------------/
-// db.sequelize.sync(syncOptions).then(function() {
-//   http.listen(PORT, function() {
-//     console.log(
-//       "Listening on port %s. http://localhost:%s/",
-//       PORT,
-//       PORT
-//     );
-//   });
-// });
 
 module.exports = app;
