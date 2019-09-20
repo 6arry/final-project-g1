4b5063ba10f66025f22283438ae12f028f811de0
gccunanan <gccunanan@gmail.com>
2019-09-20 11:21:35 -0700

    connection updates

--- server/db/index.js
+++ server/db/index.js
@@ -3,7 +3,8 @@
 const mongoose = require('mongoose')
 
 mongoose
-    .connect('mongodb://djadmin:djpassword123@ds217438.mlab.com:17438/heroku_sz4drf5d', { useNewUrlParser: true })
+    .connect('mongodb://djadmin:djpassword123@ds217438.mlab.com:17438/heroku_sz4drf5d', 
+    { useNewUrlParser: true, useUnifiedTopology: true })
     .catch(e => {
         console.error('Connection error', e.message)
     })
